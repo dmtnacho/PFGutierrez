98cd066f07c8f38786b4b72a55c569a51e3ab0b4
Ignacio Gutierrez <ignacioguti191@gmail.com>
2023-09-23 16:43:41 -0300

    Se realizan cambios para hacer el carrito funcional, actualmente solo en la seccion New Arrivals

--- js/seccion-prods.js
+++ js/seccion-prods.js
@@ -1,75 +1,135 @@
+let carrito = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Recuperar el carrito desde localStorage
+    if (localStorage.getItem('carrito')){
+        const carritoData = localStorage.getItem('carrito');
+        carrito = JSON.parse(carritoData);
+        console.table(carrito);
+
+        renderizarProds(carrito);
+    }
+    else{
+        localStorage.setItem('carrito', []);
+    }
+});
+
 const listado = document.getElementById('listado');
+renderizarProds(products);
 
 function renderizarProds(listaProds) {
-    listado.innerHTML = '';
     for (const prod of listaProds) {
-        listado.innerHTML += `
-        <div class="block__product">
-        <img src=${prod.foto} alt=${prod.nombre}>
-        <h3>${prod.nombre}</h3>
-        <p>€ ${prod.precio}</p>
-    </div>
-        `;
+        const tarjeta = document.createElement('div');
+        tarjeta.classList.add('block__product');
+
+        const imagenProducto = document.createElement('img');
+        imagenProducto.src = prod.foto;
+        imagenProducto.alt = prod.nombre;
+        imagenProducto.addEventListener('click', () => {
+            agregarCarrito(prod);
+        });
+
+        const nombreProducto = document.createElement('h3');
+        nombreProducto.textContent = prod.nombre;
+
+        const precioProducto = document.createElement('p');
+        precioProducto.textContent = `$ ${prod.precio.toLocaleString(undefined, { maximumFractionDigits: 2 })}`;
+
+
+        tarjeta.appendChild(imagenProducto);
+        tarjeta.appendChild(nombreProducto);
+        tarjeta.appendChild(precioProducto);
+
+        listado.appendChild(tarjeta);
     }
 }
 
-renderizarProds(products);
 
-// Función para agregar un producto al carrito
-function agregarAlCarrito(products) {
-    // Obtén el cuerpo de la tabla del carrito
-    var carritoBody = document.getElementById('carrito-body');
 
-    // Crea una nueva fila para el producto
-    var fila = document.createElement('tr');
 
-    // Llena la fila con la información del producto
-    fila.innerHTML = `
-        <td>
-            <div class="cart-info">
-                <img src="${prod.foto}" alt="${prod.nombre}">
-                <div>
-                    <p>${prod.nombre}</p>
-                    <small>Price: $${prod.precio}</small>
-                </div>
-            </div>
-        </td>
-        <td><input type="number" value="1"></td>
-        <td>$${prod.precio}</td>
-        <td><a href="#" class="remove-link">Remove</a></td>
-    `;
 
-    // Agrega la fila al cuerpo de la tabla
-    carritoBody.appendChild(fila);
 
-    // Agrega un manejador de eventos al enlace de eliminación para eliminar productos del carrito
-    var removeLink = fila.querySelector('.remove-link');
-    removeLink.addEventListener('click', function (event) {
-        event.preventDefault();
-        carritoBody.removeChild(fila); // Elimina la fila del carrito
-    });
+
+function agregarCarrito(producto) {
+    carrito.push(producto);
+    console.table(carrito);
+    alert(`¡${producto.nombre} se agrego al carrito exitosamente!`);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
-// Evento de clic para agregar productos al carrito
-document.addEventListener('DOMContentLoaded', function () {
-    var products = [
-        // ... Tu arreglo de productos ...
-    ];
 
-    var listado = document.getElementById('listado');
 
-    listado.addEventListener('click', function (event) {
-        var elementoClickeado = event.target;
-        var productoId = elementoClickeado.getAttribute('data-products-id');
 
-        if (productsId) {
-            var products = products.find(function (prod) {
-                return prod.id == productoId;
-            });
 
-            if (products) {
-                agregarAlCarrito(products);
-            }
-        }
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Función para agregar un producto al carrito
+// function agregarAlCarrito(products) {
+//     // Obtén el cuerpo de la tabla del carrito
+//     var carritoBody = document.getElementById('carrito-body');
+
+//     // Crea una nueva fila para el producto
+//     var fila = document.createElement('tr');
+
+//     // Llena la fila con la información del producto
+//     fila.innerHTML = `
+//         <td>
+//             <div class="cart-info">
+//                 <img src="${prod.foto}" alt="${prod.nombre}">
+//                 <div>
+//                     <p>${prod.nombre}</p>
+//                     <small>Price: $${prod.precio}</small>
+//                 </div>
+//             </div>
+//         </td>
+//         <td><input type="number" value="1"></td>
+//         <td>$${prod.precio}</td>
+//         <td><a href="#" class="remove-link">Remove</a></td>
+//     `;
+
+//     // Agrega la fila al cuerpo de la tabla
+//     carritoBody.appendChild(fila);
+
+//     // Agrega un manejador de eventos al enlace de eliminación para eliminar productos del carrito
+//     var removeLink = fila.querySelector('.remove-link');
+//     removeLink.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         carritoBody.removeChild(fila); // Elimina la fila del carrito
+//     });
+// }
+
+// // Evento de clic para agregar productos al carrito
+// document.addEventListener('DOMContentLoaded', function () {
+//     var products = [
+
+//     ];
+
+//     var listado = document.getElementById('listado');
+
+//     listado.addEventListener('click', function (event) {
+//         var elementoClickeado = event.target;
+//         var productoId = elementoClickeado.getAttribute('data-products-id');
+
+//         if (productoId) {
+//             var products = products.find(function (prod) {
+//                 return prod.id == productoId;
+//             });
+
+//             if (products) {
+//                 agregarAlCarrito(products);
+//             }
+//         }
+//     });
+// });
+
